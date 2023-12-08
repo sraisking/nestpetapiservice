f0beddeb380b5aeffa7aeaa568e256f261b78d22
Your Name <shahrukh.akhter486@gmail.com>
2023-12-08 19:17:01 +0530

    Updated Paths

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const port = process.env.PORT || 5000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //saves a lot of code , whenever validation decorator is seen it handles .
   console.log(process.env)
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(5000);
+  await app.listen(port);
 }
 bootstrap();
